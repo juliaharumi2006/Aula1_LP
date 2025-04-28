52b911459f96770c82e799382255f75a0aec1ef9
juliaharumi2006 <juharumi.nas@gmail.com>
2025-04-27 22:57:00 -0300

    Lista_5 (3/12)

---
+++ LP_Lista_05/ex3_list5/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int []vetor = {10,20,30,40,50,60,70,80,90,100};
+        int n;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro");
+        n = in.nextInt();
+
+        for(int i=0;i<=9;i++) {
+            if (vetor[i] == n) {
+                System.out.println("Esse valor existe no vetor! Sua posição é " + i);
+            }
+        }
+    }
+}
