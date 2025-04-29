a113ffa74c88d3dbe13b49f7c3624dc4894a9d6a
juliaharumi2006 <juharumi.nas@gmail.com>
2025-04-28 22:54:36 -0300

    Lista_5 (3/12) resolvido

--- LP_Lista_05/ex3_list5/src/Main.java
+++ LP_Lista_05/ex3_list5/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int []vetor = {10,20,30,40,50,60,70,80,90,100};
+        int[] vetor = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
         int n;
 
         Scanner in = new Scanner(System.in);
@@ -10,10 +10,13 @@ public class Main {
         System.out.println("Digite um número inteiro");
         n = in.nextInt();
 
-        for(int i=0;i<=9;i++) {
+        for (int i = 0; i <= 9; i++) {
             if (vetor[i] == n) {
                 System.out.println("Esse valor existe no vetor! Sua posição é " + i);
+                return; //encerra o programa
             }
         }
+
+        System.out.println("Valor não encontrado");
     }
 }
